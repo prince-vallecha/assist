7799eee50950691367a4b5ac24c3ac4185225cea
prince-vallecha <141742167+prince-vallecha@users.noreply.github.com>
2026-08-02 19:43:56 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,20 +4,26 @@ plugins {
 }
 
 android {
-    namespace = "com.prince.scheduler"
-    compileSdk = 34
+    namespace = "com.example.assist"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.prince.scheduler"
-        minSdk = 26
-        targetSdk = 34
+        applicationId = "com.example.assist"
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
-        versionName = "0.1"
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -32,7 +38,14 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
+    testImplementation("junit:junit:4.13.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
